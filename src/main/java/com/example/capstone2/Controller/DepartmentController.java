cf194f068d582fb05df13f2937f404e6e8718dee
Danah alsalmi <118121745+DanahAlsalmi@users.noreply.github.com>
2024-08-19 00:41:32 +0300

    Add files via upload

---
+++ src/main/java/com/example/capstone2/Controller/DepartmentController.java
@@ -0,0 +1,43 @@
+package com.example.capstone2.Controller;
+
+import com.example.capstone2.Model.Department;
+import com.example.capstone2.Service.DepartmentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/department")
+@RequiredArgsConstructor
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @GetMapping("/get")
+    public ResponseEntity getAllDepartments() {
+        List<Department> departments = departmentService.getAllDepartments();
+        return ResponseEntity.ok(departments);
+    }
+
+    @PostMapping("/add")
+    public ResponseEntity createDepartment(@Valid @RequestBody Department department, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.badRequest().body(errors.getAllErrors());
+        }
+         departmentService.createDepartment(department);
+        return ResponseEntity.status(201).body("Department created successfully");
+    }
+
+
+
+    @GetMapping("/by-department/{departmentName}")
+    public ResponseEntity getEmployeesByDepartment(@PathVariable String departmentName) {
+        List<Map<String, String>> employees = departmentService.getEmployeesByDepartmentName(departmentName);
+        return ResponseEntity.ok(employees);
+    }
+}
---
+++ src/main/java/com/example/capstone2/Controller/EmployeeController.java
@@ -0,0 +1,61 @@
+package com.example.capstone2.Controller;
+
+import com.example.capstone2.Model.Employee;
+import com.example.capstone2.Model.User;
+import com.example.capstone2.Service.EmployeeService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/employee")
+@RequiredArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    //Done test - Get
+    @GetMapping("/get")
+    public ResponseEntity getAllEmployees() {
+        return ResponseEntity.status(200).body(employeeService.getAllEmployees());
+    }
+
+    //Done test - Add
+    @PostMapping("/add")
+    public ResponseEntity addEmployee(@Valid @RequestBody Employee employee, Errors errors) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(400).body(errors.getFieldError().getDefaultMessage());
+        }
+        employeeService.addEmployee(employee);
+        return ResponseEntity.status(201).body("Employee added successfully");
+    }
+
+    //Update
+    @PutMapping("/update/{empId}")
+    public ResponseEntity updateEmployee(@PathVariable Integer empId,@Valid @RequestBody Employee employee,Errors errors ) {
+        if (errors.hasErrors()) {
+            return ResponseEntity.status(400).body(errors.getFieldError().getDefaultMessage());
+        }
+        employeeService.updateEmployee(empId,employee);
+        return ResponseEntity.status(201).body("Employee updated successfully");
+    }
+
+    //Delete
+    @DeleteMapping("/delete/{empId}")
+    public ResponseEntity deleteEmployee(@PathVariable Integer empId) {
+        employeeService.deleteEmployee(empId);
+        return ResponseEntity.status(201).body("Employee deleted successfully");
+    }
+
+
+    @GetMapping("/by-role/{role}")
+    public ResponseEntity getEmployeesByRole(@PathVariable String role) {
+        List<User> employees = employeeService.getEmployeesByRole(role);
+        return ResponseEntity.ok(employees);
+    }
+
+}
